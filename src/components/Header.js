02095a16bc8aed24843b2ac54407cf4c47f61233
venturads <albert@venturads.com>
2020-08-26 18:42:57 -0700

    updated header css

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,24 @@ import React from 'react';
 import '../css/Header.css';
 import icon1 from '../images/airbnb-logo.png';
 import SearchIcon from '@material-ui/icons/Search';
+import LanguageIcon from '@material-ui/icons/Language';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar } from "@material-ui/core";
 
 function Header() {
     return (
         <div className='header'>
-            <img className="header-icon" src={icon1} alt=""
+            <img className="header_icon" src={icon1} alt=""
             />
             <div className="header_center">
                 <input type="text" />
                 <SearchIcon />
+            </div>
+            <div className="header_right">
+                <p>Become a host</p>
+                <LanguageIcon />
+                <ExpandMoreIcon />
+                <Avatar />
             </div>
         </div>
     )
