d6effd0367032d7d857d0887f56ff783a718b5af
venturads <albert@venturads.com>
2020-08-26 18:27:18 -0700

    clean up css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
-//import './Header.css';
+import '../css/Header.css';
 import icon1 from '../images/airbnb-logo.png';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
         <div className='header'>
             <img className="header-icon" src={icon1} alt=""
             />
+            <div className="header_center">
+                <input type="text" />
+                <SearchIcon />
+            </div>
         </div>
     )
 }
